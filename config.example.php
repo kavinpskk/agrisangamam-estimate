38c6b46e4124bd3dc9b6f949092bc74686dcfe4a
kavinpskk <kavinpskk@gmail.com>
2026-08-30 16:46:28 +0530

    Add config.example.php

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'u495945422_estimate',
+    'db_user' => 'u495945422_estimate',
+    'db_pass' => 'CHANGE_THIS_PASSWORD',
+    'base_url' => 'https://estimate.agrisangamam.in',
+    'timezone' => 'Asia/Kolkata',
+];
